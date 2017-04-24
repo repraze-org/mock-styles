f75a1460c7e10a60f78a8f5cd442019c0cca6062
Thomas Dubosc <repraze@gmail.com>
2017-04-24 02:12:01 +0100

    Better showcase in demo

--- demo/demo.js
+++ demo/demo.js
@@ -8,45 +8,85 @@
         var stages = [
             {class: 'mkst-browser'},
             {class: 'mkst-browser mkst-alt'},
-            {class: 'mkst-browser mkst-url', data: {'mkst-url' : "hello"}},
+            {class: 'mkst-browser mkst-url mkst-alt'},
+            {class: 'mkst-browser mkst-url', data: {'mkst-url' : "http://repraze.com"}},
+            {class: 'mkst-browser mkst-url mkst-alt', data: {'mkst-url' : "http://repraze.com"}},
+
+            {class: 'mkst-app'},
+            {class: 'mkst-app mkst-alt'},
+            {class: 'mkst-app', data: {'mkst-app' : "Repraze"}},
+            {class: 'mkst-app mkst-alt', data: {'mkst-app' : "Repraze"}},
+
+            {class: 'mkst-stack'},
+            {class: 'mkst-stack mkst-alt'},
+
+            {class: 'mkst-shuffled'},
+            {class: 'mkst-shuffled mkst-alt'},
+            {class: 'mkst-shuffled mkst-nice'},
+            {class: 'mkst-shuffled mkst-nice mkst-alt'},
+            {class: 'mkst-shuffled mkst-messy'},
+            {class: 'mkst-shuffled mkst-messy mkst-alt'},
         ];
 
         return this.each(function(){
             var $this = $(this);
-            var inner = $(this).find('> *');
 
-            var n = 0;
+            var outer = $this.find('.demo-animate');
+            var inner = outer.find('> *');
+
+            var infoClass = $this.find('.demo-info > .class');
+            var infoData = $this.find('.demo-info > .data');
+
+            var n = -1;
             var nextStage = function(el){
-                el.removeClass(stages[n].class);
-                el.data({});
+                if(stages[n]){
+                    var stage = stages[n];
+                    el.removeClass(stage.class);
+                    if(stage.data){
+                        for(var key in stage.data){
+                            el.removeAttr(key);
+                        }
+                    }
+                }
                 n = (n+1)%stages.length;
-                el.addClass(stages[n].class);
-                console.log(stages[n].data);
-                if(stages[n].data){
-                    el.data(stages[n].data);
+                stage = stages[n];
+                el.addClass(stage.class);
+                infoClass.text('class="'+stage.class+'"');
+                if(stage.data){
+                    var text = [];
+                    for(var key in stage.data){
+                        var dataKey = 'data-'+key;
+                        el.attr(dataKey, stage.data[key]);
+                        text.push(dataKey+'="'+stage.data[key]+'"')
+                    }
+                    infoData.text(text.join(', '));
+                }else{
+                    infoData.text('');
                 }
             };
 
             var loop = function(){
                 inner.css({
-                    width: '200px',
-                    height: '100px'
+                    width: '300px',
+                    height: '200px'
                 });
 
                 inner
-                    .animate({width: '600px'}, settings.speed)
+                    .animate({width: '600px'}, settings.speed, function(){nextStage(outer);})
                     .delay(settings.sleep)
-                    .animate({height: '400px'}, settings.speed, function(){nextStage($this);})
+                    .animate({height: '400px'}, settings.speed, function(){nextStage(outer);})
                     .delay(settings.sleep)
-                    .animate({width: '200px'}, settings.speed)
+                    .animate({width: '300px'}, settings.speed, function(){nextStage(outer);})
                     .delay(settings.sleep)
-                    .animate({height: '100px'}, settings.speed, function(){nextStage($this);})
+                    .animate({height: '200px'}, settings.speed, function(){nextStage(outer);})
                     .delay(settings.sleep)
                     .queue(function(next){
                         loop();
                         next();
                     });
             }
+
+            nextStage(outer);
             loop();
         });
     };
